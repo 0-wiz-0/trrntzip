e08ec852bba896709594a38235b0376ca9184419
Thomas Klausner <tk@giga.or.at>
2022-04-18 17:26:03 +0200

    clang-format

--- src/platform.c
+++ src/platform.c
@@ -1,69 +1,61 @@
-//Copyright (C) 2005 TorrentZip Team (StatMat,shindakun,Ultrasubmarine,r3nh03k)
+// Copyright (C) 2005 TorrentZip Team (StatMat,shindakun,Ultrasubmarine,r3nh03k)
 //
-//This program is free software; you can redistribute it and/or
-//modify it under the terms of the GNU General Public License
-//as published by the Free Software Foundation; either
-//version 2 of the License, or (at your option) any later
-//version.
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later
+// version.
 //
-//This program is distributed in the hope that it will be useful,
-//but WITHOUT ANY WARRANTY; without even the implied warranty of
-//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//GNU General Public License for more details.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
 //
-//You should have received a copy of the GNU General Public License
-//along with this program; if not, write to the Free Software
-//Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
 #include <ctype.h>
-#include <unistd.h>
 #include <stdio.h>
+#include <unistd.h>
 
 #ifndef WIN32
 
-#include <termios.h>
 #include "platform.h"
+#include <termios.h>
 
-char *
-strlwr (char *s)
-{
-  while (*s)
-  {
-    *s = (char) tolower ((unsigned char)*s);
+char *strlwr(char *s) {
+  while (*s) {
+    *s = (char)tolower((unsigned char)*s);
     ++s;
   }
   return s;
 }
 
-
 #if defined(__CYGWIN__)
 /* Workaround for Cygwin, which is missing cfmakeraw */
 /* Pasted from man page; added in serial.c arbitrarily */
-void cfmakeraw(struct termios *termios_p)
-{
-    termios_p->c_iflag &= ~(IGNBRK|BRKINT|PARMRK|ISTRIP|INLCR|IGNCR|ICRNL|IXON);
-    termios_p->c_oflag &= ~OPOST;
-    termios_p->c_lflag &= ~(ECHO|ECHONL|ICANON|ISIG|IEXTEN);
-    termios_p->c_cflag &= ~(CSIZE|PARENB);
-    termios_p->c_cflag |= CS8;
+void cfmakeraw(struct termios *termios_p) {
+  termios_p->c_iflag &=
+      ~(IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL | IXON);
+  termios_p->c_oflag &= ~OPOST;
+  termios_p->c_lflag &= ~(ECHO | ECHONL | ICANON | ISIG | IEXTEN);
+  termios_p->c_cflag &= ~(CSIZE | PARENB);
+  termios_p->c_cflag |= CS8;
 }
 #endif /* defined(__CYGWIN__) */
 
-
-
 // Not sure if this is the best way to implement this, but it works.
-int
-getch (void)
-{
+int getch(void) {
   struct termios t, t2;
   char c = 0;
 
-  tcgetattr (1, &t);
+  tcgetattr(1, &t);
   t2 = t;
-  cfmakeraw (&t2);
-  tcsetattr (1, TCSANOW, &t2);
-  fread (&c, 1, 1, stdin);
-  tcsetattr (1, TCSANOW, &t);
+  cfmakeraw(&t2);
+  tcsetattr(1, TCSANOW, &t2);
+  fread(&c, 1, 1, stdin);
+  tcsetattr(1, TCSANOW, &t);
 
   return c;
 }
--- src/platform.h
+++ src/platform.h
@@ -1,19 +1,19 @@
-//Copyright (C) 2005 TorrentZip Team (StatMat,shindakun,Ultrasubmarine,r3nh03k)
+// Copyright (C) 2005 TorrentZip Team (StatMat,shindakun,Ultrasubmarine,r3nh03k)
 //
-//This program is free software; you can redistribute it and/or
-//modify it under the terms of the GNU General Public License
-//as published by the Free Software Foundation; either
-//version 2 of the License, or (at your option) any later
-//version.
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later
+// version.
 //
-//This program is distributed in the hope that it will be useful,
-//but WITHOUT ANY WARRANTY; without even the implied warranty of
-//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//GNU General Public License for more details.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
 //
-//You should have received a copy of the GNU General Public License
-//along with this program; if not, write to the Free Software
-//Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
 #ifndef PLATFORM_DOT_H
 #define PLATFORM_DOT_H
@@ -34,8 +34,8 @@ void cfmakeraw(struct termios *);
 
 #define DIRSEP '/'
 
-char *strlwr (char *s);
-int getch (void);
+char *strlwr(char *s);
+int getch(void);
 
 #else
 
@@ -45,4 +45,3 @@ int getch (void);
 #endif
 
 #endif
-
