52feb5dea7eec643dda3ed677976b3c0fd2f2d8e
Thomas Klausner <tk@giga.or.at>
2022-04-18 17:31:44 +0200

    clang-format

--- src/util.c
+++ src/util.c
@@ -1,20 +1,19 @@
-//Copyright (C) 2005 TorrentZip Team (StatMat,shindakun,Ultrasubmarine,r3nh03k)
+// Copyright (C) 2005 TorrentZip Team (StatMat,shindakun,Ultrasubmarine,r3nh03k)
 //
-//This program is free software; you can redistribute it and/or
-//modify it under the terms of the GNU General Public License
-//as published by the Free Software Foundation; either
-//version 2 of the License, or (at your option) any later
-//version.
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later
+// version.
 //
-//This program is distributed in the hope that it will be useful,
-//but WITHOUT ANY WARRANTY; without even the implied warranty of
-//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//GNU General Public License for more details.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
 //
-//You should have received a copy of the GNU General Public License
-//along with this program; if not, write to the Free Software
-//Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -27,66 +26,56 @@
 #include "global.h"
 #include "util.h"
 
-int
-StringCompare (const void *str1, const void *str2)
-{
-  const char **p1 = (const char **) str1;
-  const char **p2 = (const char **) str2;
-  return (strcasecmp (*p1, *p2));
+int StringCompare(const void *str1, const void *str2) {
+  const char **p1 = (const char **)str1;
+  const char **p2 = (const char **)str2;
+  return (strcasecmp(*p1, *p2));
 }
 
-int EndsWithCaseInsensitive(const char * str1, const char * str2)
-{
+int EndsWithCaseInsensitive(const char *str1, const char *str2) {
   int n1, n2;
-  n1 = strlen (str1);
-  n2 = strlen (str2);
-  if (n2<n1)
-  {
-    return strcasecmp (str1+n1-n2, str2);
+  n1 = strlen(str1);
+  n2 = strlen(str2);
+  if (n2 < n1) {
+    return strcasecmp(str1 + n1 - n2, str2);
   } else {
-    return strcasecmp (str1, str2+n2-n1);
+    return strcasecmp(str1, str2 + n2 - n1);
   }
 }
 
 // Create a dynamic string array
-char **
-DynamicStringArrayCreate (int iElements)
-{
+char **DynamicStringArrayCreate(int iElements) {
   int iCount;
   char **StringArray;
 
-  StringArray = (char **) calloc (sizeof (char *), iElements);
+  StringArray = (char **)calloc(sizeof(char *), iElements);
   if (!StringArray)
     return NULL;
 
-  for (iCount = 0; iCount < iElements; iCount++)
-  {
-    StringArray[iCount] = (char *) malloc (MAX_PATH + 1);
+  for (iCount = 0; iCount < iElements; iCount++) {
+    StringArray[iCount] = (char *)malloc(MAX_PATH + 1);
 
     // Check for error with above alloc
     // If there is, free up everything we managed to alloc
     // and return error
     if (!StringArray[iCount])
-      return DynamicStringArrayDestroy( StringArray, iCount );
+      return DynamicStringArrayDestroy(StringArray, iCount);
     StringArray[iCount][0] = 0;
   }
   return (StringArray);
 }
 
 // Destroy a dynamic string array
-char **
-DynamicStringArrayDestroy (char **StringArray, int iElements)
-{
+char **DynamicStringArrayDestroy(char **StringArray, int iElements) {
   int iCount;
 
-  CHECK_DYNAMIC_STRING_ARRAY( StringArray, iElements );
+  CHECK_DYNAMIC_STRING_ARRAY(StringArray, iElements);
 
-  for (iCount = 0; iCount < iElements; iCount++)
-  {
-    free (StringArray[iCount]);
+  for (iCount = 0; iCount < iElements; iCount++) {
+    free(StringArray[iCount]);
   }
 
-  free (StringArray);
+  free(StringArray);
 
   return NULL;
 }
@@ -94,80 +83,70 @@ DynamicStringArrayDestroy (char **StringArray, int iElements)
 // Resize a dynamic string array
 // to have iNewElements in total.
 // iNewElements may be larger or smaller than *piElements.
-char **
-DynamicStringArrayResize (char **StringArray, int *piElements, int iNewElements)
-{
+char **DynamicStringArrayResize(char **StringArray, int *piElements,
+                                int iNewElements) {
   int iCount;
   char **TmpPtr = NULL;
 
-  CHECK_DYNAMIC_STRING_ARRAY( StringArray, *piElements );
+  CHECK_DYNAMIC_STRING_ARRAY(StringArray, *piElements);
 
-  for (iCount = iNewElements; iCount < *piElements; iCount++)
-  {
-    free (StringArray[iCount]);
+  for (iCount = iNewElements; iCount < *piElements; iCount++) {
+    free(StringArray[iCount]);
   }
   TmpPtr = StringArray;
-  StringArray =
-    (char **) realloc (StringArray,
-                       iNewElements * sizeof (char *));
+  StringArray = (char **)realloc(StringArray, iNewElements * sizeof(char *));
   if (!StringArray)
-    return DynamicStringArrayDestroy (TmpPtr, *piElements);
+    return DynamicStringArrayDestroy(TmpPtr, *piElements);
 
-  for (iCount = *piElements; iCount < iNewElements; iCount++)
-  {
-    StringArray[iCount] = (char *) malloc (MAX_PATH + 1);
+  for (iCount = *piElements; iCount < iNewElements; iCount++) {
+    StringArray[iCount] = (char *)malloc(MAX_PATH + 1);
 
     // Check for error with above alloc
     // If there is, free up everything we managed to alloc
     // and return error
     if (!StringArray[iCount])
-      return DynamicStringArrayDestroy( StringArray, iCount );
+      return DynamicStringArrayDestroy(StringArray, iCount);
     StringArray[iCount][0] = 0;
   }
   *piElements = iNewElements;
 
-  CHECK_DYNAMIC_STRING_ARRAY( StringArray, *piElements );
+  CHECK_DYNAMIC_STRING_ARRAY(StringArray, *piElements);
 
   return (StringArray);
 }
 
-void DynamicStringArrayCheck (char **StringArray, int iElements)
-{
+void DynamicStringArrayCheck(char **StringArray, int iElements) {
   // All StringArray elements should be non-null,
   // because they were all allocated by DynamicStringArrayCreate.
   int i, l;
   for (i = 0; i < iElements; ++i) {
-    assert( StringArray[i] );
-    l = strlen( StringArray[i] );
-    assert( l >= 0 );
-    assert( l <= MAX_PATH );
+    assert(StringArray[i]);
+    l = strlen(StringArray[i]);
+    assert(l >= 0);
+    assert(l <= MAX_PATH);
   }
 }
 
-char *
-get_cwd (void)
-{
+char *get_cwd(void) {
   char *pszCWD = NULL;
   int cchCWD = 1024;
 
-  pszCWD = malloc (cchCWD);
+  pszCWD = malloc(cchCWD);
 
   if (!pszCWD)
     return NULL;
 
-  while (!getcwd (pszCWD, cchCWD - 2))
-  {
+  while (!getcwd(pszCWD, cchCWD - 2)) {
     cchCWD += 1024;
-    free (pszCWD);
-    pszCWD = malloc (cchCWD);
+    free(pszCWD);
+    pszCWD = malloc(cchCWD);
     if (!pszCWD)
       return NULL;
   }
 
-  cchCWD = strlen (pszCWD);
+  cchCWD = strlen(pszCWD);
 
-  if (pszCWD[cchCWD - 1] != DIRSEP)
-  {
+  if (pszCWD[cchCWD - 1] != DIRSEP) {
     pszCWD[cchCWD] = DIRSEP;
     pszCWD[cchCWD + 1] = 0;
   }
--- src/util.h
+++ src/util.h
@@ -1,35 +1,35 @@
-//Copyright (C) 2005 TorrentZip Team (StatMat,shindakun,Ultrasubmarine,r3nh03k)
+// Copyright (C) 2005 TorrentZip Team (StatMat,shindakun,Ultrasubmarine,r3nh03k)
 //
-//This program is free software; you can redistribute it and/or
-//modify it under the terms of the GNU General Public License
-//as published by the Free Software Foundation; either
-//version 2 of the License, or (at your option) any later
-//version.
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later
+// version.
 //
-//This program is distributed in the hope that it will be useful,
-//but WITHOUT ANY WARRANTY; without even the implied warranty of
-//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//GNU General Public License for more details.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
 //
-//You should have received a copy of the GNU General Public License
-//along with this program; if not, write to the Free Software
-//Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
 #ifndef UTIL_DOT_H
 #define UTIL_DOT_H
 
-int StringCompare (const void *str1, const void *str2);
-int EndsWithCaseInsensitive(const char * str1, const char * str2);
-char **DynamicStringArrayCreate (int iElements);
-char **DynamicStringArrayDestroy (char **StringArray, int iElements);
-char **DynamicStringArrayResize (char **StringArray, int *piElements, int iNewElements);
-void DynamicStringArrayCheck (char **StringArray, int iElements);
+int StringCompare(const void *str1, const void *str2);
+int EndsWithCaseInsensitive(const char *str1, const char *str2);
+char **DynamicStringArrayCreate(int iElements);
+char **DynamicStringArrayDestroy(char **StringArray, int iElements);
+char **DynamicStringArrayResize(char **StringArray, int *piElements,
+                                int iNewElements);
+void DynamicStringArrayCheck(char **StringArray, int iElements);
 #ifdef NDEBUG
-#define CHECK_DYNAMIC_STRING_ARRAY( StringArray, iElements ) \
-	(void*)0
+#define CHECK_DYNAMIC_STRING_ARRAY(StringArray, iElements) (void *)0
 #else
-#define CHECK_DYNAMIC_STRING_ARRAY( StringArray, iElements ) \
-	DynamicStringArrayCheck( StringArray, iElements )
+#define CHECK_DYNAMIC_STRING_ARRAY(StringArray, iElements)                     \
+  DynamicStringArrayCheck(StringArray, iElements)
 #endif
-char *get_cwd (void);
+char *get_cwd(void);
 #endif
