93ebaf53f65589dd006e3b29ff0ae06d9c9c1038
Thomas Klausner <tk@giga.or.at>
2022-04-18 17:21:17 +0200

    clang-format

--- src/global.h
+++ src/global.h
@@ -1,19 +1,19 @@
-//Copyright (C) 2005 TorrentZip Team (StatMat,shindakun,Ultrasubmarine,r3nh03k)
+// Copyright (C) 2005 TorrentZip Team (StatMat,shindakun,Ultrasubmarine,r3nh03k)
 //
-//This program is free software; you can redistribute it and/or
-//modify it under the terms of the GNU General Public License
-//as published by the Free Software Foundation; either
-//version 2 of the License, or (at your option) any later
-//version.
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later
+// version.
 //
-//This program is distributed in the hope that it will be useful,
-//but WITHOUT ANY WARRANTY; without even the implied warranty of
-//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//GNU General Public License for more details.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
 //
-//You should have received a copy of the GNU General Public License
-//along with this program; if not, write to the Free Software
-//Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
 #ifndef GLOBAL_DOT_H
 #define GLOBAL_DOT_H
@@ -31,8 +31,7 @@
 
 #define MAX_PATH 1024
 
-typedef struct _WORKSPACE
-{
+typedef struct _WORKSPACE {
   zip_fileinfo zi;
   char **FileNameArray;
   int iElements;
@@ -43,8 +42,7 @@ typedef struct _WORKSPACE
   FILE *fErrorLog;
 } WORKSPACE;
 
-typedef struct _MIGRATE
-{
+typedef struct _MIGRATE {
   unsigned int cEncounteredDirs, cEncounteredZips;
   unsigned int cRezippedZips, cOkayZips, cErrorZips;
   double ExecTime;
