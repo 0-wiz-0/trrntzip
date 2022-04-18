5d7eb8fa858d74a0260adcea7b1bae928791516d
Thomas Klausner <tk@giga.or.at>
2022-04-18 17:25:40 +0200

    clang-format

--- src/logging.c
+++ src/logging.c
@@ -1,33 +1,33 @@
-//Copyright (C) 2005 TorrentZip Team (StatMat,shindakun,Ultrasubmarine,r3nh03k)
+// Copyright (C) 2005 TorrentZip Team (StatMat,shindakun,Ultrasubmarine,r3nh03k)
 //
-//This program is free software; you can redistribute it and/or
-//modify it under the terms of the GNU General Public License
-//as published by the Free Software Foundation; either
-//version 2 of the License, or (at your option) any later
-//version.
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later
+// version.
 //
-//This program is distributed in the hope that it will be useful,
-//but WITHOUT ANY WARRANTY; without even the implied warranty of
-//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//GNU General Public License for more details.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
 //
-//You should have received a copy of the GNU General Public License
-//along with this program; if not, write to the Free Software
-//Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <string.h>
-#include <stdio.h>
-#include <time.h>
-#include <stdarg.h>
 #include <ctype.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <time.h>
 #include <unistd.h>
 
-#include "util.h"
 #include "logging.h"
+#include "util.h"
 
-static FILE *OpenLog (const char *szFileName);
+static FILE *OpenLog(const char *szFileName);
 
 // Global var to store if the logprint func is expecting more
 // data to complete a line. This prevents the timestamp being
@@ -35,105 +35,92 @@ static FILE *OpenLog (const char *szFileName);
 char continueline = 0;
 
 // Used to print to screen and file at the same time
-void
-logprint (FILE * stdf, FILE * f, char *format, ...)
-{
+void logprint(FILE *stdf, FILE *f, char *format, ...) {
   time_t now;
   struct tm *t;
   va_list arglist;
-  char szTimeBuffer[2048+1];
-  char szMessageBuffer[2048+1];
+  char szTimeBuffer[2048 + 1];
+  char szMessageBuffer[2048 + 1];
 
   // Only print the timestamp if this is the beginning of a line
-  if (!continueline)
-  {
-    now = time (NULL);
-    t = localtime (&now);
+  if (!continueline) {
+    now = time(NULL);
+    t = localtime(&now);
 
-    sprintf (szTimeBuffer, "[%04d/%02d/%02d - %02d:%02d:%02d] ",
-             t->tm_year + 1900, t->tm_mon + 1, t->tm_mday,
-             t->tm_hour, t->tm_min, t->tm_sec);
+    sprintf(szTimeBuffer, "[%04d/%02d/%02d - %02d:%02d:%02d] ",
+            t->tm_year + 1900, t->tm_mon + 1, t->tm_mday, t->tm_hour, t->tm_min,
+            t->tm_sec);
   } else {
     szTimeBuffer[0] = 0;
   }
 
   // Look for a newline in the passed data
-  continueline = !strchr (format, '\n');
+  continueline = !strchr(format, '\n');
 
-  va_start (arglist, format);
-  vsprintf (szMessageBuffer, format, arglist);
-  va_end (arglist);
+  va_start(arglist, format);
+  vsprintf(szMessageBuffer, format, arglist);
+  va_end(arglist);
 
   // Print to stdout or stderr
-  if (stdf)
-  {
-    fprintf (stdf, "%s", szMessageBuffer);
+  if (stdf) {
+    fprintf(stdf, "%s", szMessageBuffer);
     fflush(stdf);
   }
 
   // Print to logfile
-  if (f)
-  {
-    fprintf (f, "%s%s", szTimeBuffer, szMessageBuffer);
-    fflush (f);
+  if (f) {
+    fprintf(f, "%s%s", szTimeBuffer, szMessageBuffer);
+    fflush(f);
   }
-
 }
 
 // Used to print to screen and two files at the same time
-void
-logprint3 (FILE * stdf, FILE * f1, FILE * f2, char *format, ...)
-{
+void logprint3(FILE *stdf, FILE *f1, FILE *f2, char *format, ...) {
   time_t now;
   struct tm *t;
   va_list arglist;
-  char szTimeBuffer[2048+1];
-  char szMessageBuffer[2048+1];
+  char szTimeBuffer[2048 + 1];
+  char szMessageBuffer[2048 + 1];
 
   // Only print the timestamp if this is the beginning of a line
-  if (!continueline)
-  {
-    now = time (NULL);
-    t = localtime (&now);
+  if (!continueline) {
+    now = time(NULL);
+    t = localtime(&now);
 
-    sprintf (szTimeBuffer, "[%04d/%02d/%02d - %02d:%02d:%02d] ",
-             t->tm_year + 1900, t->tm_mon + 1, t->tm_mday,
-             t->tm_hour, t->tm_min, t->tm_sec);
+    sprintf(szTimeBuffer, "[%04d/%02d/%02d - %02d:%02d:%02d] ",
+            t->tm_year + 1900, t->tm_mon + 1, t->tm_mday, t->tm_hour, t->tm_min,
+            t->tm_sec);
   } else {
     szTimeBuffer[0] = 0;
   }
 
   // Look for a newline in the passed data
-  continueline = !strchr (format, '\n');
+  continueline = !strchr(format, '\n');
 
-  va_start (arglist, format);
-  vsprintf (szMessageBuffer, format, arglist);
-  va_end (arglist);
+  va_start(arglist, format);
+  vsprintf(szMessageBuffer, format, arglist);
+  va_end(arglist);
 
   // Print to stdout or stderr
-  if (stdf)
-  {
-    fprintf (stdf, "%s", szMessageBuffer);
+  if (stdf) {
+    fprintf(stdf, "%s", szMessageBuffer);
   }
 
   // Print to logfile 1
-  if (f1)
-  {
-    fprintf (f1, "%s%s", szTimeBuffer, szMessageBuffer);
-    fflush (f1);
+  if (f1) {
+    fprintf(f1, "%s%s", szTimeBuffer, szMessageBuffer);
+    fflush(f1);
   }
 
   // Print to logfile 2
-  if (f2)
-  {
-    fprintf (f2, "%s%s", szTimeBuffer, szMessageBuffer);
-    fflush (f2);
+  if (f2) {
+    fprintf(f2, "%s%s", szTimeBuffer, szMessageBuffer);
+    fflush(f2);
   }
 }
 
-int
-OpenProcessLog (const char * pszWritePath, const char * pszRelPath, MIGRATE * mig)
-{
+int OpenProcessLog(const char *pszWritePath, const char *pszRelPath,
+                   MIGRATE *mig) {
   int iPathLen = 0;
   time_t now;
   struct tm *t;
@@ -141,52 +128,46 @@ OpenProcessLog (const char * pszWritePath, const char * pszRelPath, MIGRATE * mi
   char szRelPathBuf[MAX_PATH + 1];
   char *pszDirname = NULL;
 
-  now = time (NULL);
-  t = localtime (&now);
+  now = time(NULL);
+  t = localtime(&now);
 
-  if (pszRelPath)
-  {
-    iPathLen = strlen (pszRelPath);
+  if (pszRelPath) {
+    iPathLen = strlen(pszRelPath);
 
-    if(iPathLen > 1)
-    {
-      sprintf (szRelPathBuf, "%s", pszRelPath);
+    if (iPathLen > 1) {
+      sprintf(szRelPathBuf, "%s", pszRelPath);
       // Strip off the last slash
-      if (szRelPathBuf[iPathLen-1] == DIRSEP) {
+      if (szRelPathBuf[iPathLen - 1] == DIRSEP) {
         szRelPathBuf[iPathLen - 1] = 0;
       }
 
       pszDirname = strrchr(szRelPathBuf, DIRSEP);
-      if (pszDirname)
-      {
+      if (pszDirname) {
         pszDirname++;
       } else {
         pszDirname = szRelPathBuf;
       }
 
-      if(szRelPathBuf[iPathLen - 2] == ':')
-      {
+      if (szRelPathBuf[iPathLen - 2] == ':') {
         szRelPathBuf[iPathLen - 2] = 0;
         pszDirname = szRelPathBuf;
       }
-    }
-    else
-    {
-      sprintf (szRelPathBuf, "%s", "");
+    } else {
+      sprintf(szRelPathBuf, "%s", "");
       // FIXME!
       // Note that pszDirname is not assigned!
-      // Therefore szLogname comes up as (null), which is a nice kind of undefined behaviour!
+      // Therefore szLogname comes up as (null), which is a nice kind of
+      // undefined behaviour!
     }
   }
 
-  sprintf (szLogname, "%s[%s]_[%04d-%02d-%02d - %02d-%02d-%02d]",
-           pszWritePath, pszDirname, t->tm_year + 1900, t->tm_mon + 1, t->tm_mday,
-           t->tm_hour, t->tm_min, t->tm_sec);
+  sprintf(szLogname, "%s[%s]_[%04d-%02d-%02d - %02d-%02d-%02d]", pszWritePath,
+          pszDirname, t->tm_year + 1900, t->tm_mon + 1, t->tm_mday, t->tm_hour,
+          t->tm_min, t->tm_sec);
 
-  mig->fProcessLog = OpenLog (szLogname);
+  mig->fProcessLog = OpenLog(szLogname);
 
-  if (mig->fProcessLog)
-  {
+  if (mig->fProcessLog) {
     fprintf(mig->fProcessLog, "TorrentZip processing logfile for : \"%s\"\n",
             szRelPathBuf);
   }
@@ -194,45 +175,38 @@ OpenProcessLog (const char * pszWritePath, const char * pszRelPath, MIGRATE * mi
   return mig->fProcessLog ? TZ_OK : TZ_CRITICAL;
 }
 
-FILE *
-OpenErrorLog (char qGUILaunch)
-{
+FILE *OpenErrorLog(char qGUILaunch) {
   struct stat istat;
   int rc;
 
-  rc = stat ("error.log", &istat);
+  rc = stat("error.log", &istat);
 
-  if (!rc && istat.st_size && !qGUILaunch)
-  {
-    fprintf (stderr,
-             "There is a previous 'error.log'. Are you sure you have dealt\n"
-             "with the problems encountered last time this program was run?\n"
-             "(Press 'y' to continue or any other key to exit.)\n\n");
+  if (!rc && istat.st_size && !qGUILaunch) {
+    fprintf(stderr,
+            "There is a previous 'error.log'. Are you sure you have dealt\n"
+            "with the problems encountered last time this program was run?\n"
+            "(Press 'y' to continue or any other key to exit.)\n\n");
 
-    if (tolower (getch ()) != 'y')
-    {
-      fprintf (stderr, "Exiting.\n");
+    if (tolower(getch()) != 'y') {
+      fprintf(stderr, "Exiting.\n");
       return NULL;
     }
   }
 
-  return (OpenLog ("error"));
+  return (OpenLog("error"));
 }
 
-static FILE *
-OpenLog (const char *szFileName)
-{
+static FILE *OpenLog(const char *szFileName) {
   FILE *f = NULL;
-  char szLog[strlen (szFileName) + 5];
+  char szLog[strlen(szFileName) + 5];
 
-  strcpy (szLog, szFileName);
-  strcat (szLog, ".log");
+  strcpy(szLog, szFileName);
+  strcat(szLog, ".log");
 
-  f = fopen (szLog, "w");
+  f = fopen(szLog, "w");
 
-  if (!f)
-  {
-    fprintf (stderr, "Could not open log file '%s'!\n", szLog);
+  if (!f) {
+    fprintf(stderr, "Could not open log file '%s'!\n", szLog);
   }
 
   return f;
--- src/logging.h
+++ src/logging.h
@@ -1,28 +1,29 @@
-//Copyright (C) 2005 TorrentZip Team (StatMat,shindakun,Ultrasubmarine,r3nh03k)
+// Copyright (C) 2005 TorrentZip Team (StatMat,shindakun,Ultrasubmarine,r3nh03k)
 //
-//This program is free software; you can redistribute it and/or
-//modify it under the terms of the GNU General Public License
-//as published by the Free Software Foundation; either
-//version 2 of the License, or (at your option) any later
-//version.
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later
+// version.
 //
-//This program is distributed in the hope that it will be useful,
-//but WITHOUT ANY WARRANTY; without even the implied warranty of
-//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//GNU General Public License for more details.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
 //
-//You should have received a copy of the GNU General Public License
-//along with this program; if not, write to the Free Software
-//Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
 #ifndef LOGGING_DOT_H
 #define LOGGING_DOT_H
 
 #include "global.h"
 
-int OpenProcessLog (const char * pszWritePath, const char * pszRelPath, MIGRATE * mig);
-FILE *OpenErrorLog (char qGUILaunch);
-void logprint (FILE * stdf, FILE * f, char *format, ...);
-void logprint3 (FILE * stdf, FILE * f1, FILE * f2, char *format, ...);
+int OpenProcessLog(const char *pszWritePath, const char *pszRelPath,
+                   MIGRATE *mig);
+FILE *OpenErrorLog(char qGUILaunch);
+void logprint(FILE *stdf, FILE *f, char *format, ...);
+void logprint3(FILE *stdf, FILE *f1, FILE *f2, char *format, ...);
 
 #endif
