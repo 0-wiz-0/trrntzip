0-wiz-0/trrntzip

commit 379e6cccd21b9c4d52510cddc75569341bc33435

Remove unused variable, fix indent, add cast for ctype(3) function

src/trrntzip.c
@@ -169,7 +169,6 @@ GetFileList (unzFile UnZipHandle, char **FileNameArray, int *piElements)
 {
   int rc = 0;
   int iCount = 0;
-  char **TmpPtr = NULL;
 
   unz_file_info64 ZipInfo;
 
@@ -209,10 +208,8 @@ CheckZipStatus (unz64_s * UnzipStream, WORKSPACE * ws)
   unsigned long checksum, target_checksum = 0;
   off_t ch_length = UnzipStream->size_central_dir;
   off_t ch_offset = UnzipStream->central_pos - UnzipStream->size_central_dir;
-  off_t i = 0;
   char comment_buffer[COMMENT_LENGTH + 1];
   char *ep = NULL;
-  unsigned char x;
   FILE *f = (FILE *) UnzipStream->filestream;
 
   // Quick check that the file at least appears to be a zip file.
@@ -804,17 +801,13 @@ RecursiveMigrateDir (const char *pszRelPath, WORKSPACE * ws)
   int rc = 0;
 
   char szTmpBuf[MAX_PATH + 1];
-  char szRelPathBuf[MAX_PATH + 1];
   int iElements = 0;
   char **FileNameArray = NULL;
   int iCounter = 0;
-  int n;
   int FileNameStartPos;
 
   DIR *dirp = NULL;
-  struct dirent *direntp = NULL;
   struct stat istat;
-
   MIGRATE mig;
 
   memset (&mig, 0, sizeof (MIGRATE));
@@ -949,7 +942,6 @@ main (int argc, char **argv)
   int iCount = 0;
   int iOptionsFound = 0;
   int rc = 0;
-  char *ptr = NULL;
   char szStartPath[MAX_PATH + 1];
   char szErrorLogFileName[MAX_PATH + 1];
 

src/platform.c
@@ -29,7 +29,7 @@ strlwr (char *s)
 {
   while (*s)
   {
-    *s = (char) tolower (*s);
+    *s = (char) tolower ((unsigned char)*s);
     ++s;
   }
   return s;

src/zip.c
@@ -423,8 +423,8 @@ local ZPOS64_T zip64local_SearchCentralDir(const zlib_filefunc64_32_def* pzlib_f
         break;
       }
 
-      if (uPosFound!=0)
-        break;
+    if (uPosFound!=0)
+      break;
   }
   TRYFREE(buf);
   return uPosFound;

commit 76edf318e5dea4b536094cba4db392924e9275e1

Restore variable used on WIN32
In main(), a variable "ptr" is used in an "#ifdef WIN32" block but
never declared. Restore the decaration and adjust the scope of the
"#ifdef" such that the variable is always used (though constant on
other OSes).

This should fix a build error on Windows.

Fixes: 379e6cccd21b ("Remove unused variable, fix indent, add cast for ctype(3) function")

src/trrntzip.c
@@ -914,6 +914,7 @@ int RecursiveMigrateTop(const char *pszRelPath, WORKSPACE *ws) {
 
 int main(int argc, char **argv) {
   WORKSPACE *ws;
+  char *ptr = NULL;
   int iCount = 0;
   int iOptionsFound = 0;
   int rc = 0;
@@ -998,6 +999,7 @@ int main(int argc, char **argv) {
   // the exe, it will use the user's "Documents and Settings"
   // dir if we don't do this.
   ptr = strrchr(argv[0], DIRSEP);
+#endif
   if (ptr) {
     *ptr = '\0';
     snprintf(szStartPath, sizeof(szStartPath), "%s", argv[0]);
@@ -1005,9 +1007,6 @@ int main(int argc, char **argv) {
   } else {
     pszStartPath = get_cwd();
   }
-#else
-  pszStartPath = get_cwd();
-#endif
 
   if (!pszStartPath) {
     fprintf(stderr, "Could not get startup path!\n");