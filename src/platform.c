0-wiz-0/trrntzip

commit d50049494d7675934d4eb60d22e9054fffb0f854

Add a mkstemp() implementation for WIN32

src/platform.c
@@ -15,14 +15,41 @@
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
+#include "platform.h"
+
+#ifdef WIN32
+#include <io.h>
+#include <stdlib.h>
+#include <string.h>
+
+int mkstemp(char *ntemplate) {
+  int i, fd = -1;
+  char *copy = strdup(ntemplate);
+
+  for (i = 0; i < 10; i++) {
+    if (!mktemp(ntemplate))
+      break;
+
+    fd = open(ntemplate, O_RDWR | C_CREAT | O_EXCL, S_IREAD | S_IWRITE);
+    if (fd >= 0 || errno != EEXIST)
+      break;
+
+    if (!copy) {
+      errno = ENOMEM;
+      break;
+    }
+    strcpy(ntemplate, copy);
+  }
+  free(copy);
+  return fd;
+}
+
+#else
+
 #include <ctype.h>
 #include <stdio.h>
-#include <unistd.h>
-
-#ifndef WIN32
-
-#include "platform.h"
 #include <termios.h>
+#include <unistd.h>
 
 char *strlwr(char *s) {
   while (*s) {

src/platform.h
@@ -45,6 +45,8 @@ int getch(void);
 #define stat _stati64
 #define lstat stat
 #define off_t off64_t
+
+int mkstemp(char *ntemplate);
 #endif
 
 #ifndef HAVE_FOPEN64

commit 352fe467e822f87a50e14b909ddcb0a44af8f22a

platform.c: WIN32: Fix typo in O_CREAT, add headers for file constants
Fixes: d50049494d76 ("Add a mkstemp() implementation for WIN32")

src/platform.c
@@ -17,10 +17,12 @@
 
 #include "platform.h"
 
-#ifdef WIN32
+#ifdef _WIN32
+#include <fcntl.h>
 #include <io.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
 
 int mkstemp(char *ntemplate) {
   int i, fd = -1;
@@ -30,7 +32,7 @@ int mkstemp(char *ntemplate) {
     if (!mktemp(ntemplate))
       break;
 
-    fd = open(ntemplate, O_RDWR | C_CREAT | O_EXCL, S_IREAD | S_IWRITE);
+    fd = open(ntemplate, O_RDWR | O_CREAT | O_EXCL, S_IREAD | S_IWRITE);
     if (fd >= 0 || errno != EEXIST)
       break;
 