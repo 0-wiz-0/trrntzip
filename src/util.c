0-wiz-0/trrntzip

commit f8fcedd0e0a4c31fc12f251be6e5bb3fb38422a2

Split out code that moves a new zip file in place to a new utility function
Create new function UpdateFile() in util.c to handle renaming the
temporary file to the replaced zip file. That keeps the OS-specific
details out of (the very long) MigrateZip() to improve readability.

src/util.c
@@ -15,9 +15,11 @@
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
 #include <unistd.h>
 
 #define NDEBUG
@@ -177,4 +179,43 @@ char *get_cwd(void) {
   }
 
   return pszCWD;
+}
+
+// Replaces file dest with tmpfile.
+// Returns NULL on success or an error message.
+const char *UpdateFile(const char *dest, const char *tmpfile) {
+#ifdef WIN32
+  // On WIN32, rename() fails if the destination exists. So we have to remove
+  // the destination first.
+  if (remove(dest)) {
+    if (remove(tmpfile))
+      return "Unable to remove either destination or temporary file. "
+             "Please replace the file manually.";
+    else
+      return "Unable to remove destination for replacement (temporary "
+             "file removed)."
+  }
+  if (rename(tmpfile, dest))
+    return "The original file has already been deleted, so you must rename "
+           "this file manually.";
+
+#else // !WIN32, assume POSIX semantics
+
+  // Try to preserve permissions but ignore errors
+  struct stat st;
+  if (!stat(dest, &st))
+    chmod(tmpfile, st.st_mode & ~S_IFMT);
+
+  // On a POSIX system, rename() atomically replaces the destination if
+  // it exists.
+  if (rename(tmpfile, dest)) {
+    if (remove(tmpfile))
+      return "Also could not remove temporary file. "
+             "Please replace the file manually.";
+    else
+      return strerror(errno);
+  }
+#endif
+
+  return NULL; // success
 }

src/trrntzip.c
@@ -648,51 +648,21 @@ int MigrateZip(const char *zip_path, const char *pDir, WORKSPACE *ws,
 
   rc = zipClose(ZipHandle, szTmpBuf, zip64);
 
-  if (rc != UNZ_OK) {
+  if (rc == UNZ_OK) {
+    const char *pErr = UpdateFile(szZipFileName, szTmpZipFileName);
+    if (pErr) {
+      logprint3(stderr, mig->fProcessLog, ws->fErrorLog,
+                "!!!! Could not rename temporary file \"%s\" to \"%s\". %s\n",
+                szTmpZipFileName, szZipFileName, pErr);
+      return TZ_CRITICAL;
+    }
+  } else {
     logprint3(
         stderr, mig->fProcessLog, ws->fErrorLog,
         "Unable to close temporary zip file \"%s\" - cannot process \"%s\"!\n",
         szTmpZipFileName, szZipFileName);
     remove(szTmpZipFileName);
     return TZ_ERR;
-  }
-
-#ifdef WIN32
-  // This is here because rename does not atomically replace
-  // an existing destination on WIN32.
-  if (remove(szZipFileName)) {
-    logprint3(
-        stderr, mig->fProcessLog, ws->fErrorLog,
-        "!!!! Unable to remove \"%s\" for replacement with rezipped copy.\n",
-        szZipFileName);
-    if (remove(szTmpZipFileName))
-      logprint3(stderr, mig->fProcessLog, ws->fErrorLog,
-                "!!!! Could not remove temporary file \"%s\".\n",
-                szTmpZipFileName);
-
-    return TZ_ERR;
-  }
-#endif
-
-  // rename atomically replaces the destination if it exists.
-  if (rename(szTmpZipFileName, szZipFileName)) {
-#ifdef WIN32
-    logprint3(
-        stderr, mig->fProcessLog, ws->fErrorLog,
-        "!!!! Could not rename temporary file \"%s\" to \"%s\". The original "
-        "file has already been deleted, so you must rename this file "
-        "manually.\n",
-        szTmpZipFileName, szZipFileName);
-#else
-    logprint3(stderr, mig->fProcessLog, ws->fErrorLog,
-              "!!!! Could not rename temporary file \"%s\" to \"%s\".\n",
-              szTmpZipFileName, szZipFileName);
-    if (remove(szTmpZipFileName))
-      logprint3(stderr, mig->fProcessLog, ws->fErrorLog,
-                "!!!! Could not remove temporary file \"%s\".\n",
-                szTmpZipFileName);
-#endif
-    return TZ_CRITICAL;
   }
 
   logprint(stdout, mig->fProcessLog,

src/util.h
@@ -21,7 +21,9 @@
 int CanonicalCmp(const char *s1, const char *s2);
 int StringCompare(const void *str1, const void *str2);
 int BasenameCompare(const void *str1, const void *str2);
+
 int EndsWithCaseInsensitive(const char *str, const char *tail);
+
 char **DynamicStringArrayCreate(int iElements);
 char **DynamicStringArrayDestroy(char **StringArray, int iElements);
 char **DynamicStringArrayResize(char **StringArray, int *piElements,
@@ -33,5 +35,7 @@ void DynamicStringArrayCheck(char **StringArray, int iElements);
 #define CHECK_DYNAMIC_STRING_ARRAY(StringArray, iElements)                     \
   DynamicStringArrayCheck(StringArray, iElements)
 #endif
+
 char *get_cwd(void);
+const char *UpdateFile(const char *dest, const char *tmpfile);
 #endif

commit 836e4b17d37280edf3762ed6fc2d72a49079d64d

util.c: Add missing semicolon in WIN32-only code
Fixes: f8fcedd0e0a4 ("Split out code that moves a new zip file in place to a new utility function")

src/util.c
@@ -226,7 +226,7 @@ const char *UpdateFile(const char *dest, const char *tmpfile) {
              "Please replace the file manually.";
     else
       return "Unable to remove destination for replacement (temporary "
-             "file removed)."
+             "file removed).";
   }
   if (rename(tmpfile, dest))
     return "The original file has already been deleted, so you must rename "